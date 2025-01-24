1b5f606ceae863bb3cb4c8a9f17b448e791ff538
songcw8 <90672619+songcw8@users.noreply.github.com>
2025-01-24 18:12:33 +0900

    수정1

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 async function makeIssue() {
-    const token = process.env.GITHUB_TOKEN;
-    //이것
-    const OWNER = "songcw8"; // 깃허브 계정이름
-    //이것
-    const REPO = "github_power_actions"; // 현재 레퍼지토리 이름
-    const response = await fetch(`https://api.github.com/repos/${OWNER}/${REPO}/issues`,{
+    const token = process.env.GH_TOKEN; // 주의! 일치해야함 (env와 일치해야함)
+    // 요고 1
+    const OWNER = "songcw8"; // 여러분들의 github 계정 이름
+    // 요고 2
+    const REPO = "github_power_actions"; // 여러분들의 현재 리포지터리 이름
+    const response = await fetch(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
         method: 'POST',
         headers: {
             Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
-            title : "행운의 숫자", //이것
-            body : `${Math.floor(Math.random() * 100) + 1}`, //이것
+            title: "행운의 숫자", // 요고 3
+            body: `${Math.floor(Math.random() * 100) + 1}`, // 요고 4
         })
     });
-    if(response.ok){
+    if (response.ok) {
         console.log("성공");
-    }else{
+    } else {
         console.log("실패");
     }
 }
